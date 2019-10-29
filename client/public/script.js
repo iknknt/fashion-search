28a61d234ed2ac377ac93deca72f3be38385680f
Kenta Ikeno <ken1467@icloud.com>
2019-10-29 19:57:49 +0900

    HTMLで画面を作成

---
+++ client/public/script.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('#login-show').click(function(){
+      $('#login-modal').fadeIn();
+    });
+
+
+    $('.close-modal').click(function(){
+      $('#login-modal').fadeOut();
+    });
+    
+    
+  });
