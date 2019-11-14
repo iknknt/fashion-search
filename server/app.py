7517e9320ba9a05313a559538216b7aa4e9c36a8
Kenta Ikeno <ken1467@icloud.com>
2019-11-14 14:05:49 +0900

    11/14 （成功？）

--- server/app.py
+++ server/app.py
@@ -1,27 +1,13 @@
-# FLASK_APP=app.py FLASK_DEBUG=1 flask run
+import os
 
-from flask import Flask, request
-
-from aaa import Aaa
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return 'hello flask'
-
-
-@app.route('/aaa', methods=["GET", "POST"])
-def aaa():
-    print(request.files)
-    print(request.form)
-
-    if(request.form['text'] == 'ggg'):
-        return 'gggがきました'
-    else:
-        aaa = Aaa()
-        return aaa.hello_method()
+def hello_world():
+    target = os.environ.get('TARGET', 'World')
+    return 'Hello {}!\n'.format(target)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True,host='0.0.0.0')
